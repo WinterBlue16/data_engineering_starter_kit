210c35792129b79f978c73552c582f174e220a17
Winter <winter@glocalizeinc.com>
2023-06-16 08:05:59 +0000

    [MOD]Session2 mission 3 upload

---
+++ Session2/M03_top_10_gross_revenue.sql
@@ -0,0 +1,13 @@
+select
+    userid,
+    sum(coalesce(amount,0)) as gross_revenue
+from
+(
+    select *
+    from raw_data.user_session_channel a
+    left join raw_data.session_timestamp b on a.sessionid=b.sessionid
+    left join raw_data.session_transaction c on b.sessionid=c.sessionid
+    
+)
+group by userid
+order by gross_revenue desc;
